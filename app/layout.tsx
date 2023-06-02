2fe5346c489b1c901bdb2976841ed5c6f29f49f2
aman-senpai <jemscollin1@gmail.com>
2023-06-02 13:05:32 +0530

    Finished layout, careat Header, Button, ListItem components. Create images public directory

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import Sidebar from "@/components/Sidebar";
 import "./globals.css";
 import { Figtree } from "next/font/google";
+import Home from "./(site)/page";
 
 const font = Figtree({ subsets: ["latin"] });
 
