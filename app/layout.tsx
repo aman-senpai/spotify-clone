3f5da7f2fe9d1d49000aefd34e67d18764985f4e
aman-senpai <jemscollin1@gmail.com>
2023-06-03 08:22:29 +0530

    Create supabase types, Providers for for auth and supabase

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,9 @@
 import Sidebar from "@/components/Sidebar";
 import "./globals.css";
 import { Figtree } from "next/font/google";
-import Home from "./(site)/page";
+import SupabaseProvider from "@/providers/SupabaseProvider";
+import UserProvider from "@/providers/UserProvider";
+import ModalProvider from "@/providers/ModalProvider";
 
 const font = Figtree({ subsets: ["latin"] });
 
@@ -18,7 +20,12 @@ export default function RootLayout({
     return (
         <html lang="en">
             <body className={font.className}>
-                <Sidebar>{children}</Sidebar>
+                <SupabaseProvider>
+                    <UserProvider>
+                        <ModalProvider />
+                        <Sidebar>{children}</Sidebar>
+                    </UserProvider>
+                </SupabaseProvider>
             </body>
         </html>
     );
